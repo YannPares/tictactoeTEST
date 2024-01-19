c4d40ee646491204fb5987bcac97fcc30763ce77
Devskiller <owl@devskiller.com>
2024-01-19 17:46:55 +0000

    Candidate repo initialization

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// TO-DO: Implement game below
+
+function getTiles(){
+    return document.getElementsByClassName('tile')
+}
+const tiles = getTiles()
+
+function getPlayers(){
+    return document.getElementsByClassName('player')
+}
+const players = getPlayers()
